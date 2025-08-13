f14bea8cb8debc1ff581a6a0ed33b11d3ba8bbd8
rohit <rohitmahto062@gmail.com>
2025-08-13 21:22:10 +0530

    build the foundation

---
+++ App.js
@@ -0,0 +1,7 @@
+import react from "react";
+import reactDOM from "react-dom/client";
+
+const heading = <h1>Hello World</h1>;
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
