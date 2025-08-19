3a94b3d2d173b15a2d4dcf4cf345d2d462365102
rohit <rohitmahto062@gmail.com>
2025-08-19 23:01:04 +0530

    feat: enhance restaurant card component with dynamic image and updated data

--- App.js
+++ App.js
@@ -23,12 +23,17 @@ const Header = () => {
 };
 
 const RestorentCard = ({ resData }) => {
-  const { name, cuisines, avgRating, costForTwo } = resData.info;
+  const { name, cuisines, avgRating, costForTwo, cloudinaryImageId } =
+    resData.info;
   return (
-    <div className="card-container flex-1 hover:border rounded-2xl overflow-hidden min-w-[200px] md:max-w-[250px] cursor-pointer">
-      <div className="card-image w-full">
+    <div className="card-container flex-1 hover:border rounded-2xl overflow-hidden min-w-[200px] md:max-w-[250px] cursor-pointer h-auto">
+      <div className="card-image w-full h-[200px]">
         <img
-          src="https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg"
+          className="w-full h-full object-cover"
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+            cloudinaryImageId
+          }
           alt="food"
         />
       </div>
@@ -42,35 +47,35 @@ const RestorentCard = ({ resData }) => {
   );
 };
 
-const resObj = [
+const resList = [
   {
     info: {
-      id: "1003414",
-      name: "Pizza Hut",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/a1e023e4-bdf3-4bcb-b4d9-2498ba52528e_1003414.JPG",
-      locality: "Chhindwara",
-      areaName: "Chhindwara City",
-      costForTwo: "₹350 for two",
-      cuisines: ["Pizzas"],
-      avgRating: 4.3,
-      parentId: "721",
-      avgRatingString: "4.3",
-      totalRatingsString: "172",
+      id: "234875",
+      name: "Adil Hotel",
+      cloudinaryImageId: "gp1ityra6utvzqn6ghnv",
+      locality: "Rautha Wada",
+      areaName: "Chhindwara Locality",
+      costForTwo: "₹150 for two",
+      cuisines: ["Biryani", "Tandoor"],
+      avgRating: 4.4,
+      parentId: "27123",
+      avgRatingString: "4.4",
+      totalRatingsString: "1.3K+",
       sla: {
-        deliveryTime: 38,
-        lastMileTravel: 13.1,
+        deliveryTime: 29,
+        lastMileTravel: 11.5,
         serviceability: "SERVICEABLE",
-        slaString: "35-40 mins",
-        lastMileTravelString: "13.1 km",
+        slaString: "25-30 mins",
+        lastMileTravelString: "11.5 km",
         iconType: "ICON_TYPE_EMPTY",
       },
       availability: {
-        nextCloseTime: "2025-08-19 00:00:00",
+        nextCloseTime: "2025-08-20 00:00:00",
         opened: true,
       },
       badges: {},
       isOpen: true,
+      aggregatedDiscountInfoV2: {},
       type: "F",
       badgesV2: {
         entityBadges: {
@@ -79,20 +84,85 @@ const resObj = [
           textExtendedBadges: {},
         },
       },
-      aggregatedDiscountInfoV3: {
-        header: "ITEMS",
-        subHeader: "AT ₹99",
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
       },
-      orderabilityCommunication: {
-        title: {},
-        subTitle: {},
-        message: {},
-        customIcon: {},
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+
+  {
+    info: {
+      id: "151515",
+      name: "Gupta Bhojnalay",
+      cloudinaryImageId: "jo9pdipf4elcuch8g55q",
+      locality: "fulwara chowk",
+      areaName: "Chhindwara Locality",
+      costForTwo: "₹200 for two",
+      cuisines: ["Fast Food", "Indian", "Beverages"],
+      avgRating: 4.1,
+      veg: true,
+      parentId: "91635",
+      avgRatingString: "4.1",
+      totalRatingsString: "452",
+      sla: {
+        deliveryTime: 37,
+        lastMileTravel: 11.8,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "11.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-08-19 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹999",
+        discountTag: "FLAT DEAL",
       },
       differentiatedUi: {
         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
         differentiatedUiMediaDetails: {
-          mediaType: "ADS_MEDIA_ENUM_IMAGE",
           lottie: {},
           video: {},
         },
@@ -108,14 +178,268 @@ const resObj = [
       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
     },
     analytics: {
-      context: "seo-data-c44b8889-6e0d-41b6-8ecf-3f6f467da5c1",
+      context: "seo-data-7f04b67b-4452-4705-9c4c-6cb751e9094d",
     },
     cta: {
-      link: "https://www.swiggy.com/city/chhindwara/pizza-hut-chhindwara-city-rest1003414",
-      text: "RESTAURANT_MENU",
+      link: "https://www.swiggy.com/city/chhindwara/gupta-bhojnalay-fulwara-chowk-chhindwara-locality-rest151515",
       type: "WEBLINK",
     },
-    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "658210",
+      name: "The Fusion Lounge",
+      cloudinaryImageId: "fa4944f0cfdcbca2bec1f3ab8e3db3f7",
+      locality: "Triloki nagar",
+      areaName: "Railway Station",
+      costForTwo: "₹300 for two",
+      cuisines: [
+        "South Indian",
+        "Chinese",
+        "Beverages",
+        "Fast Food",
+        "Desserts",
+      ],
+      avgRating: 4.1,
+      parentId: "395453",
+      avgRatingString: "4.1",
+      totalRatingsString: "357",
+      sla: {
+        deliveryTime: 46,
+        lastMileTravel: 11.9,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "11.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-08-19 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "522144",
+      name: "Blue Chilli Restaurant",
+      cloudinaryImageId: "je6qlse8csxgcfpuoybe",
+      locality: "Ambedkar Nagar",
+      areaName: "Prasia Road",
+      costForTwo: "₹150 for two",
+      cuisines: ["South Indian", "Indian", "Fast Food"],
+      avgRating: 4,
+      veg: true,
+      parentId: "311804",
+      avgRatingString: "4.0",
+      totalRatingsString: "227",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 10.9,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "10.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-08-19 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "625927",
+      name: "Kathi Junction",
+      cloudinaryImageId: "nigqvxgzvyxtfjuqasgg",
+      locality: "Bunglow Madhuvan Colony",
+      areaName: "Prasia Road",
+      costForTwo: "₹200 for two",
+      cuisines: ["rolls", "Burgers", "Pizzas", "Fast Food"],
+      avgRating: 4.3,
+      parentId: "1935",
+      avgRatingString: "4.3",
+      totalRatingsString: "390",
+      sla: {
+        deliveryTime: 39,
+        lastMileTravel: 12.2,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "12.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-08-19 23:15:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "15% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "184424",
+      name: "Sab Ghar Tak Foods",
+      cloudinaryImageId: "ryzswg44bt7thqzkuaj4",
+      locality: "Sanchar Colony",
+      areaName: "Parsia Road",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "North Indian",
+        "South Indian",
+        "Chinese",
+        "Beverages",
+        "Fast Food",
+        "Desserts",
+      ],
+      avgRating: 4.1,
+      parentId: "173932",
+      avgRatingString: "4.1",
+      totalRatingsString: "617",
+      sla: {
+        deliveryTime: 48,
+        lastMileTravel: 14.1,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "14.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-08-19 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹50 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
   },
 ];
 
@@ -127,7 +451,7 @@ const Body = () => {
       </div>
       <div className="restorent-container ml-5 mr-5">
         <div className="restorent-card flex gap-4 flex-wrap">
-          {resObj.map((restaurant) => (
+          {resList.map((restaurant) => (
             <RestorentCard key={restaurant.info.id} resData={restaurant} />
           ))}
         </div>
