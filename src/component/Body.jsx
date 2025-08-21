56ee1c5634961e96541d758d7c45074e70904bde
rohit <rohitmahto062@gmail.com>
2025-08-21 23:39:21 +0530

    feat: update Body component to include search functionality for top-rated restaurants

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,15 +1,27 @@
 import RestorentCard from "./RestorentCard";
 import resList from "../util/dumyData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(resList);
   return (
     <div id="body-container">
       <div className="search-container">
-        <input type="text" placeholder="Search" />
+        <button
+          className="p-2 border m-1.5 rounded-md bg-amber-500 font-bold cursor-pointer"
+          onClick={() => {
+            const filteredResturents = resList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setRestaurants(filteredResturents);
+          }}
+        >
+          Top Rated Restorent
+        </button>
       </div>
       <div className="restorent-container ml-5 mr-5">
         <div className="restorent-card flex gap-4 flex-wrap">
-          {resList.map((restaurant) => (
+          {restaurants.map((restaurant) => (
             <RestorentCard key={restaurant.info.id} resData={restaurant} />
           ))}
         </div>
