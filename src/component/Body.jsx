5a955a0a485973976ad365c90ee8e74d00d62008
rohit <rohitmahto062@gmail.com>
2025-08-24 16:23:54 +0530

    feat: implement restaurant data fetching and enhance restaurant card display

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,14 +1,41 @@
 import RestorentCard from "./RestorentCard";
 import resList from "../util/dumyData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState(resList);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    const url =
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83631&tags=layout_CCS_Pizza&sortBy=&filters=&type=rcv2&offset=0&page_type=null";
+    const data = await fetch(proxyUrl + url);
+
+    const json = await data.json();
+    // console.log(json?.data?.cards[9]?.card?.card?.info);
+
+    const restaurantsData =
+      json?.data?.cards
+        ?.filter((card) => card?.card?.card?.info)
+        .map((card) => card?.card?.card?.info) || [];
+
+    // ------Remove duplicate restaurants based on their ID------
+    // const uniqueRestaurants = Array.from(
+    //   new Map(restaurantsData.map((item) => [item.id, item])).values()
+    // );
+
+    // console.log(restaurantsData);
+    setRestaurants(restaurantsData);
+  };
   return (
     <div id="body-container">
       <div className="filter-container">
         <button
-          className="p-2 border m-1.5 rounded-md bg-amber-500 font-bold cursor-pointer"
+          className="p-2 border m-1.5 rounded-md bg-amber-500 font-bold cursor-pointer hover:bg-amber-300"
           onClick={() => {
             const filteredResturents = resList.filter(
               (res) => res.info.avgRating > 4
@@ -22,7 +49,7 @@ const Body = () => {
       <div className="restorent-container ml-5 mr-5">
         <div className="restorent-card flex gap-4 flex-wrap">
           {restaurants.map((restaurant) => (
-            <RestorentCard key={restaurant.info.id} resData={restaurant} />
+            <RestorentCard key={restaurant?.id} resData={restaurant} />
           ))}
         </div>
       </div>
--- src/component/RestorentCard.jsx
+++ src/component/RestorentCard.jsx
@@ -1,22 +1,25 @@
 import { RES_URL } from "../util/constant";
 
 const RestorentCard = ({ resData }) => {
-  const { name, cuisines, avgRating, costForTwo, cloudinaryImageId } =
-    resData.info;
+  // console.log(resData);
+  const { name, cloudinaryImageId, cuisines, avgRating, costForTwo } = resData;
   return (
     <div className="card-container flex-1 hover:border rounded-2xl overflow-hidden min-w-[200px] md:max-w-[250px] cursor-pointer h-auto shadow-md">
       <div className="card-image w-full h-[200px]">
         <img
           className="w-full h-full object-cover"
           src={RES_URL + cloudinaryImageId}
+          // src={image}
           alt="food"
         />
       </div>
       <div className="card-content m-2">
         <h3>{name}</h3>
-        <p>{cuisines.join(", ")}</p>
-        <p>Rating: {avgRating}</p>
-        <p>{costForTwo}</p>
+        <p> {cuisines.join(", ")} </p>
+        <p>Rating: {avgRating} </p>
+        <p>
+          <strong> {costForTwo} s</strong>
+        </p>
       </div>
     </div>
   );
